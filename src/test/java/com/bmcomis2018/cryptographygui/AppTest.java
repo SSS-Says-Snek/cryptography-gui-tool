55f9ebb84505c04b4c49e5834059a522ecf5efdc
Brandon <bmcomis2018@gmail.com>
2024-02-10 00:30:14 -0600

    Initial commit

---
+++ src/test/java/com/bmcomis2018/cryptographygui/AppTest.java
@@ -0,0 +1,20 @@
+package com.bmcomis2018.cryptographygui;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
